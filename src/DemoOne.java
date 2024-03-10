a544949cae028e2816ed7376758a7e5223abc62c
singhdharmveer311 <rs92539@gmail.com>
2024-03-10 08:46:52 +0100

    This is first commit

--- src/DemoOne.java
+++ src/DemoOne.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class DemoOne {
+public class DemoOne {
+
+    public static void main(String[] args) {
+        System.out.println("Dharmveer Singh - This is Java Selenium Practice");
+    }
 }
